50487ced33ace663557c3069f908409352ef0a5f
abigail <abigailnankamba@gmail.com>
2020-01-29 12:49:29 +0200

    Feature: Added the Entities(City and Country)

---
+++ Entities/CityEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Cities;
+
+namespace Entities
+{
+    public class CityEntity : CityModel
+    {
+        [Key]
+        public int CityID { get; set; }
+
+        public virtual Cities.CountryEntity Country { get; set; }
+    }
+}
---
+++ Entities/CountryEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Cities;
+
+namespace Entities
+{
+    class CountryEntity : CountryModel
+    {
+        [Key]
+        public int CountryID { get; set; }
+
+        public virtual List<CityEntity> Cities { get; set; }
+    }
+}
