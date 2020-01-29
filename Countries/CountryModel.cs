ef24c16acfe24b255276e278b01150b13eab1a40
abigail <abigailnankamba@gmail.com>
2020-01-29 12:12:35 +0200

    Feature: Added the Models(City and country Models)

---
+++ Countries/CountryModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Models
+{
+    public class CountryModel
+    {
+        public string Name { get; set; }
+        public string ISO2 { get; set; }
+        public string ISO3 { get; set; }
+    }
+}
